92f7af4a4b64bd7fd6bda2ffcb443491b55f63e5
bellbabl <hathaitatbanjungthong@gmail.com>
2025-04-21 00:27:07 +0700

    fix

--- src/features/users/types.ts
+++ src/features/users/types.ts
@@ -2,13 +2,14 @@ export interface User {
   id: number;
   firstName: string;
   lastName: string;
+  department: string;
   gender: string;
   age: number;
   address: {
     postalCode: string;
   };
 }
-  
+
 export interface TransformedUserData {
   user: {
     male: number;
